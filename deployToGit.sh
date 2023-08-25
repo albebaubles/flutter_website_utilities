e25aadef57c3d3d451453dd0f5b633229040e9a2
albebaubles <al@albebaubles.com>
2023-08-25 15:28:35 -0700

    cleaup of code

---
+++ deployToGit.sh
@@ -0,0 +1,4 @@
+cp -R /Users/acorbett/dev/flutter_website_utilities/build/web/ /Users/acorbett/dev/albebaubles.github.io 
+cd /Users/acorbett/dev/albebaubles.github.io 
+git add .
+git commit -am 'deploy' && git push
