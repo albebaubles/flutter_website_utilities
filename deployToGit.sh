f5356d6bc521e86c7ad8c2386e2f521b65259cb0
Al Corbett <al@albebaubles.com>
2025-11-24 10:41:24 -0700

    removing deploy script

--- deployToGit.sh
+++
@@ -1,5 +0,0 @@
-flutter build web --web-renderer auto
-cp -R /Users/acorbett/dev/flutter_website_utilities/build/web/ /Users/acorbett/dev/albebaubles.github.io 
-cd /Users/acorbett/dev/albebaubles.github.io 
-git add .
-git commit -am 'deploy' && git push
